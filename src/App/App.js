d02361aba39212588c61bd1fdee78824042fbbe7
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-03-10 08:45:06 +0300

    Starting project

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+    return (
+        <div className="app">
+            <div className="window">
+
+            </div>
+        </div>
+    )
+}
+
+export default App;
